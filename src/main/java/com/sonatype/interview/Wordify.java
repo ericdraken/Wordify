fdd29da1bbb2272c1d9771c8b8d65ac0ee4f17b1
Eric Draken <eric@freshtrackscanada.com>
2019-04-18 13:56:14 -0700

    Initial commit

---
+++ src/main/java/com/sonatype/interview/Wordify.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019. Eric Draken - ericdraken.com
+ */
+
+package com.sonatype.interview;
+
+public class Wordify
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/sonatype/interview/WordifyTest.java
@@ -0,0 +1,17 @@
+package com.sonatype.interview;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Wordify.
+ */
+public class WordifyTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
